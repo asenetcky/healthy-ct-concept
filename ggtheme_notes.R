da62aaa921f7f768de1f205c5f05f0c591abda26
asenetcky <asenetcky@gmail.com>
2025-01-07 11:15:35 -0500

    factored grp

--- ggtheme_notes.R
+++ ggtheme_notes.R
@@ -47,34 +47,6 @@ theme_ct_vertical <- function() {
   )
 }
 
-
-#testing a random theme 
-blue_theme <- function() {
-  theme(
-    # add border 1)
-    panel.border = element_rect(colour = "blue", fill = NA, linetype = 2),
-    # color background 2)
-    panel.background = element_rect(fill = "aliceblue"),
-    # modify grid 3)
-    panel.grid.major.x = element_line(colour = "steelblue", linetype = 3, size = 0.5),
-    panel.grid.minor.x = element_blank(),
-    panel.grid.major.y =  element_line(colour = "steelblue", linetype = 3, size = 0.5),
-    panel.grid.minor.y = element_blank(),
-    # modify text, axis and colour 4) and 5)
-    axis.text = element_text(colour = "steelblue", face = "italic", family = "Times New Roman"),
-    axis.title = element_text(colour = "steelblue", family = "Times New Roman"),
-    axis.ticks = element_line(colour = "steelblue"),
-    # legend at the bottom 6)
-    legend.position = "bottom"
-  )
-}
-
-ggplot(cars, aes(x = wt, y = mpg)) +
-  geom_point() +
-  blue_theme()
-
-
-
 # classic
 ggplot(cars, aes(x = wt, y = mpg)) +
   geom_point() +
@@ -123,7 +95,10 @@ demo_food <-
       17.0
     )
   ) |> 
-  dplyr::mutate(year = factor(year))
+  dplyr::mutate(
+    year = factor(year),
+    grp = factor(grp, levels = c("CT", "US", "CT Children", "US Children"))
+  )
 
 
 
